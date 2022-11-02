d6194a24b19958bb9d881667300a3d5ceed41d93
Kishan Dewangan <76161356+Kishandew@users.noreply.github.com>
2022-11-02 19:50:42 +0530

    Create Data_Science_Skills.sql

---
+++ Solution/Easy/Data_Science_Skills.sql
@@ -0,0 +1,16 @@
+--Data Science Skill
+
+SELECT
+    candidate_id
+FROM
+    candidates
+WHERE
+    skill in ('Python','Tableau','PostgreSQL')
+GROUP BY 
+    candidate_id
+HAVING 
+    COUNT(skill)=3
+ORDER BY 
+    candidate_id
+
+--Solved By kishan Kumar
