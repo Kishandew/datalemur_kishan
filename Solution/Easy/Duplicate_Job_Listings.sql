07f7d3a80fce4bbe3077dc4d4ed1e602f66f402e
Kishan Dewangan <76161356+Kishandew@users.noreply.github.com>
2022-12-01 21:59:42 +0530

    Create Duplicate_Job_Listings.sql

---
+++ Solution/Easy/Duplicate_Job_Listings.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*) FROM (
+SELECT company_id, COUNT(company_id), title, description
+FROM job_listings
+GROUP BY company_id, title, description
+HAVING COUNT(company_id) > 1) AS duplicate_companies;
