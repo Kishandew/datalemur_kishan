f9265877ddc29d31e1faa4d0de7b9ad19ec46270
Kishan Dewangan <76161356+Kishandew@users.noreply.github.com>
2022-12-01 22:03:57 +0530

    Update Duplicate_Job_Listings.sql

--- Solution/Easy/Duplicate_Job_Listings.sql
+++ Solution/Easy/Duplicate_Job_Listings.sql
@@ -1,5 +1,9 @@
+--Duplicate Job Listings
+
 SELECT COUNT(*) FROM (
 SELECT company_id, COUNT(company_id), title, description
 FROM job_listings
 GROUP BY company_id, title, description
 HAVING COUNT(company_id) > 1) AS duplicate_companies;
+
+--Solved By Kishan Kumar
