1033846728dcec43d98c2061480e5f81ba194518
Kishan Dewangan <76161356+Kishandew@users.noreply.github.com>
2022-12-09 18:13:18 +0530

    Update Average_Post_Hiatus_Part1.sql

--- Solution/Easy/Average_Post_Hiatus_Part1.sql
+++ Solution/Easy/Average_Post_Hiatus_Part1.sql
@@ -1,7 +1,8 @@
 -- Average_Post_Hiatus_Part1
 
 SELECT user_id,EXTRACT(day from MAX(post_date)-min(post_date)) days_between
-FROM posts WHERE post_date BETWEEN '2021-01-01' AND '2021-12-31'
+FROM posts 
+WHERE post_date BETWEEN '2021-01-01' AND '2021-12-31'
 GROUP BY user_id
 HAVING EXTRACT(day from MAX(post_date)-min(post_date)) NOT in ('0')
 
